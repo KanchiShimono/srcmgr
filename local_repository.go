42ad201723142e5343c4ce3e6b39985b89bad65d
Kanchi Shimono <shimono-kanchi-yc@ynu.jp>
2017-03-12 04:41:32 +0900

    Change local repository path from GOPATH to srcmgr.root set by .gitconfig

--- local_repository.go
+++ local_repository.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 )
@@ -38,14 +39,48 @@ func LocalRepositoryPath(fullPath string) (path *LocalRepository, err error) {
 }
 
 func localRepositoryRoots() (roots []string) {
+	buf, err := exec.Command(
+		"git",
+		"config",
+		"--path",
+		"--null",
+		"--get-all",
+		"srcmgr.root").Output()
 
-	srcRoot := os.Getenv("GOPATH")
-	if srcRoot != "" {
-		srcRoot = filepath.Join(srcRoot, "src")
-		roots = filepath.SplitList(srcRoot)
-	} else {
-		err := errors.New("GOPATH in not difined")
+	// TODO
+	// I have to fix following source code to continue running if err =! nil.
+	// Now program will stop when above git command return error.
+	// However I have to accept the path execting srcmgr.root written in .gitconfig
+	// I will fix ASAP with highest priority.
+	if err != nil {
+		fmt.Println("srcmgr root has not been set in .gitconfig")
 		panic(err)
+	}
+
+	// cfroots := strings.TrimRight(string(buf), "\000")
+	roots = strings.Split(strings.TrimRight(string(buf), "\000"), "\000")
+
+	if len(roots) == 0 {
+		srcRoot := os.Getenv("GOPATH")
+		if srcRoot != "" {
+			srcRoot = filepath.Join(srcRoot, "src")
+			roots = filepath.SplitList(srcRoot)
+		} else {
+			err := errors.New("GOPATH in not difined")
+			panic(err)
+		}
+	}
+
+	for i, v := range roots {
+		path := filepath.Clean(v)
+		if _, err := os.Stat(path); err == nil {
+			roots[i], err = filepath.EvalSymlinks(path)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			roots[i] = path
+		}
 	}
 
 	return roots
