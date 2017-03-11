068cc256fa21f0346380e7ead9a47e7d77416c0b
Kanchi Shimono <shimono-kanchi-yc@ynu.jp>
2017-03-12 04:48:54 +0900

    Change root paths for list command

--- commands.go
+++ commands.go
@@ -111,55 +111,57 @@ func Get(c *cli.Context) error {
 func List(c *cli.Context) error {
 	printRelPath := c.Bool("rel-path")
 	printDeepPath := c.Bool("deep-path")
-	srcRoot := os.Getenv("GOPATH")
+	srcRoots := localRepositoryRoots()
 
-	if srcRoot == "" {
-		return util.ShowNewError("GOPATH is not found")
-	}
-
-	srcRoot = filepath.Join(srcRoot, "src")
-
-	var localRepositories []*LocalRepository
-
-	filepath.Walk(srcRoot, func(path string, info os.FileInfo, err error) error {
-		if info == nil || info.IsDir() == false || err != nil {
-			return nil
+	for _, srcRoot := range srcRoots {
+		if srcRoot == "" {
+			return util.ShowNewError("srcmgr root directory is not found")
 		}
 
-		existVCSDir := false
-		var VCSDIR = []string{".git", ".hg", ".svn"}
+		// srcRoot = filepath.Join(srcRoot, "src")
 
-		for _, vcs := range VCSDIR {
-			file, err := os.Stat(filepath.Join(path, vcs))
-			if err == nil && file.IsDir() {
-				existVCSDir = true
-				break
+		var localRepositories []*LocalRepository
+
+		filepath.Walk(srcRoot, func(path string, info os.FileInfo, err error) error {
+			if info == nil || info.IsDir() == false || err != nil {
+				return nil
 			}
-		}
 
-		if !existVCSDir {
-			return nil
-		}
+			existVCSDir := false
+			var VCSDIR = []string{".git", ".hg", ".svn"}
 
-		repo, err := LocalRepositoryPath(path)
-		if err != nil || repo == nil {
-			return nil
-		}
+			for _, vcs := range VCSDIR {
+				file, err := os.Stat(filepath.Join(path, vcs))
+				if err == nil && file.IsDir() {
+					existVCSDir = true
+					break
+				}
+			}
 
-		localRepositories = append(localRepositories, repo)
+			if !existVCSDir {
+				return nil
+			}
 
-		if printDeepPath {
-			return nil
-		}
+			repo, err := LocalRepositoryPath(path)
+			if err != nil || repo == nil {
+				return nil
+			}
 
-		return filepath.SkipDir
-	})
+			localRepositories = append(localRepositories, repo)
 
-	for _, repo := range localRepositories {
-		if printRelPath {
-			fmt.Println(repo.RelPath)
-		} else {
-			fmt.Println(repo.FullPath)
+			if printDeepPath {
+				return nil
+			}
+
+			return filepath.SkipDir
+		})
+
+		for _, repo := range localRepositories {
+			if printRelPath {
+				fmt.Println(repo.RelPath)
+			} else {
+				fmt.Println(repo.FullPath)
+			}
 		}
 	}
 
