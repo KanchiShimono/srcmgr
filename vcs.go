2fd50af4ca123708308e64ec30230f449cfe776a
Kanchi Shimono <shimono-kanchi-yc@ynu.jp>
2017-04-27 12:31:11 +0900

    Implement git struct

--- vcs.go
+++ vcs.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"github.com/KanchiShimono/srcmgr/util"
+)
+
 type VCS interface {
 	Clone(remoteRepo RemotoRepository, local string) (err error)
+}
+
+type Git struct{}
+
+func (vcs *Git) Clone(remoteRepo RemotoRepository, local string) (err error) {
+	return util.Run("git", "clone", remoteRepo.StringURL(), local)
 }
